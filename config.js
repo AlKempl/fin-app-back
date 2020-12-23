28dec77aaa55843906552f386b06b90344e7b6d9
alkempl <alkempled@gmail.com>
2020-12-23 15:56:42 +0300

    Fix sec

--- config.js
+++ config.js
@@ -3,15 +3,13 @@ require('dotenv').config()
 const {Pool} = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgres://kgqjbddwuyzhgm:2bcecef14aa7dcf7adbb3a62df15f69cd07caacc4afe8faf476cf059ac02b13d@ec2-54-155-22-153.eu-west-1.compute.amazonaws.com:5432/dbc25ri2361c0p`
-
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
 })
 
-const API_KEY = isProduction? process.env.API_KEY : 'novocaineikeepitcoming';
+const API_KEY = process.env.API_KEY;
 
 module.exports = {pool, API_KEY}
